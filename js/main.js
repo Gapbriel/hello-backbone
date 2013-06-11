15fe74078120ed19cd19afbe316e6b0a0e3c44f9
Gaston Mira <gastonmira@gmail.com>
2013-06-11 14:04:36 -0300

    subiendo archivos nuevos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+require.config({
+  paths: {
+    jquery: 'libs/jquery/jquery-min',
+    underscore: 'libs/underscore/underscore-min',
+    backbone: 'libs/backbone/backbone-min',
+    backboneLocalStorage: 'libs/backbonelocalStorage/backbone.localStorage'
+  }
+
+});
+
+require([
+  // Load our app module and pass it to our definition function
+  'app',
+
+], function(App){
+  // The "app" dependency is passed in as "App"
+  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
+  App.initialize();
+});
