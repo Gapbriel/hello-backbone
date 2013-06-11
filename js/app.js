785938f10ba24dc60d1d3599f111bc6ebb272a85
nicolasmartello <nicolasmartello@yahoo.com>
2013-06-11 15:11:39 -0300

    Hello world !!
    modifique para que puedamos arrancar

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ define([
   'jquery', 
   'underscore', 
   'backbone',
-  'router', // Request router.js
 ], function($, _, Backbone, Router){
   var initialize = function(){
-    // Pass in our Router module and call it's initialize function
-    Router.initialize();
+    
+    console.log( 'hello worold!!!' );
+
   };
 
   return { 
