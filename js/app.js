9d3d4debe0b1dfc6d75766978e245a9dc0a2dcbd
nicolasmartello <nicolasmartello@yahoo.com>
2013-06-12 10:24:14 -0300

    objeto Movie
    Toque el index cambie el id del form

--- js/app.js
+++ js/app.js
@@ -5,9 +5,42 @@ define([
   'backbone',
 ], function($, _, Backbone, Router){
   var initialize = function(){
-    
-    console.log( 'hello worold!!!' );
 
+     var Movies = Backbone.Model.extend({
+        defaults:{            
+            titulo:"Vacio",
+            duracion:0,
+            genero:"Vacio",
+            sinopsis:"Vacio"
+        }
+      };
+
+      var MoviesView = Backbone.View.extend({
+        tagName:"div",
+        className:"form",
+        template:$("#moviesTmp").html(),// cambiar el id en index.html
+
+        render:function () {
+            var tmpl = _.template(this.template); //tmp toma Json y retorna HTML
+
+            this.$el.html(tmpl(this.model.toJSON()));
+            return this;
+        },
+
+        events: {
+            "click .delete": "deleteMovie"
+        },
+
+        deleteBook:function () {
+            //Delete model
+            this.model.destroy();
+
+            //Delete view
+            this.remove();
+        }
+    });
+
+     
   };
 
   return { 
