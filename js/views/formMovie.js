26011c2d618122e0ab5acf884b1ea66c824e7481
nicolasmartello <nicolasmartello@yahoo.com>
2013-06-13 12:49:41 -0300

    FormMovie - HTML

--- js/views/formMovie.js
+++ js/views/formMovie.js
@@ -1,7 +1,25 @@
-define( function(require) {
+define( [
+	'jquery',
+	'underscore',
+	'backbone',
+	'text!templates/formMovie.html'
+	],	function($, _, Backbone, formViewTemplate) {
 
 	var FormMovieView = Backbone.View.extend({
           
-     });
+          el:$(".form"),
+
+          render:function(){
+          	///var data = {}; NO LE PASO NINGUNA DATA
+
+          	var compiledTemplate = _.template( formViewTemplate );
+          	
+          	this.$el.append( compiledTemplate );
+
+          }
+          
+      });
+
+	return FormMovieView;
 
 });
