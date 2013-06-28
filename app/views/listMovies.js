3aa544e84acc7a1d1ed4488117139feddb2d4674
nicolasmartello <nicolasmartello@yahoo.com>
2013-06-28 16:39:59 -0300

    pagination fixed

--- app/views/listMovies.js
+++ app/views/listMovies.js
@@ -5,61 +5,65 @@ define([
 	'models/movie',
 	'text!../templates/listsMovies.html',
 	'text!../templates/footerPagination.html'
-	], function($,_, Backbone, Movie,Template, FooterPagination){
+	], function($,_, Backbone, Movie,Template, PageTemplate){
 		
-		var pagesCount = 5;  
+		var pagesCount = 3, currentPage;  
 		
 		var MovieListView = Backbone.View.extend({
 			
 			render:function(page){
 
 				if( page === undefined)
-					page = 1;
+					currentPage = 1;
+				else
+					currentPage = page;
 
-				var compiledTemplate = _.template( Template);                
-                //recorro la collecion y cargo los datos en el template
-                //dos maneras distintas, en esta el each dentro de la view
-                /*_.each(this.collectionMovies.models, function(item){
-	                
-	               this.$el.append( compiledTemplate( item.attributes ) );
-
-	            }, this);*/
-				var items = this.collectionMovies.pagination(pagesCount,page);
-
-				//en este caso el each esta dentro del template			
-				this.$el.html(compiledTemplate({
-	                movies: items//this.collectionMovies.toJSON()
-	            }));
-	            
-				//this.pagination(page);
+	            this.TableMovie();
+				
+				this.PageFooter();
 
 	            return this;
             },
 
-            pagination: function (page){
+            TableMovie: function (){
+            	
+            	var compiledTemplate = _.template( Template );                                
 
-            	var footerTemplate = _.template( FooterPagination ); 
+				var items = this.collectionMovies.pagination(pagesCount,currentPage);
+				
+				this.$el.html(compiledTemplate({
+	                
+	                movies: items//this.collectionMovies.toJSON()
 
-            	var pages = Math.ceil(this.collection.length/pagesCount);
+	            }));
+
+            },
+
+            PageFooter: function (){
+            	
+            	var fTemp = _.template( PageTemplate ); 
+
+            	var pages = Math.ceil( this.collectionMovies.length / pagesCount );
 
             	var arr = [];
-                var selectPage = false;
+
                 for( var i = 1 ; i <= pages ; i++ )
                 {
                     
-                    if(i==page)
-                        selectPage = true;
+                    if(i == currentPage )
+                        selectPage = "select";
                     else
-                    	selectPage = false;
+                    	selectPage = "normal";
                     
-                    arr.push({ page: i, select_page:selectPage });
+                    arr.push({ pageNumber: i, class_page:selectPage });
                 }
-
-            	this.$el.find('tfoot td').html(compiledTemplate({
-	            
-	                pages: arr//this.collectionMovies.toJSON()
-	            
-	            }));
+                
+                
+            	this.$el.find('tfoot td').html(
+            	
+            	 	fTemp({ listPages: arr })
+            	
+            	);
 
             }
 
