43bd924405a18e7cdde4879b1459c45f52f17093
Gaston Mira <gastonmira@gmail.com>
2013-07-01 10:52:29 -0300

    modificando el type a text en html y modificando el validate en el formMovie.js

--- app/models/movie.js
+++ app/models/movie.js
@@ -19,10 +19,27 @@ define([
 				//console.log('entro en validate',options.error);
 				//return "no se guardo";
 				console.log(attrs.duration);
+			    
+				if ( attrs.title=== undefined ) {
+			      console.log("fallo el titulo");
+			      return "write a title";
+			    }
+
+			    if ( attrs.genre === undefined ) {
+			      console.log("fallo el genero");
+			      return "write a genre you mother fucker!";
+			    }
+
 			    if ( isNaN(attrs.duration) ) {
 			      console.log("fallo la duracion");
 			      return "can't end before it starts";
 			    }
+
+			    if ( attrs.sinopsis === undefined ) {
+			      console.log("fallo el genero");
+			      return "write a sinopsis you mother fucker!";
+			    }
+
 			  
 			},
 
