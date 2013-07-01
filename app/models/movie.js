95711b00d5d7214cbbd5654c63760011d9c2f1a3
Gaston Mira <gastonmira@gmail.com>
2013-07-01 12:57:38 -0300

    terminado el tema errores

--- app/models/movie.js
+++ app/models/movie.js
@@ -21,22 +21,22 @@ define([
 				console.log(attrs.duration);
 			    
 				if ( attrs.title=== "" ) {
-			      console.log("fallo el titulo");
+			     
 			      return "write a title!";
 			    }
 
 			    if ( attrs.genre === "" ) {
-			      console.log("fallo el genero");
+			      
 			      return "write a genre you mother fucker!";
 			    }
 
 			    if ( isNaN(attrs.duration) || attrs.duration === "" ) {
-			      console.log("fallo la duracion");
+			      
 			      return "can't end before it starts";
 			    }
 
 			    if ( attrs.sinopsis === "" ) {
-			      console.log("fallo el genero");
+			      
 			      return "write a sinopsis you mother fucker!";
 			    }
 
@@ -45,7 +45,7 @@ define([
 
 			initialize: function () {
 				this.on("invalid", function(model, error){
-            	 $('.error-msg').text('ERROR').show();
+            	 $('.error-msg').text('Error en un campo').show();
         		});
 
 			}
